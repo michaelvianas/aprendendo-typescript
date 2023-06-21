7d425359b2425fe5df279643fbd7f49847ccdfc0
Michael Viana <vmichael116@gmail.com>
2023-06-21 18:39:02 +0000

    Remodelando o código da propriedade de classe e método listar de negociacoes.ts

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes{
-  private negociacoes: Array<Negociacao> = [];
+  private negociacoes: Negociacao[] = [];
 
   adicionar(negociacao: Negociacao){
     this.negociacoes.push(negociacao);
   }
 
-  listar(): ReadonlyArray<Negociacao>{
+  listar(): readonly Negociacao[]{
     return this.negociacoes;
   }
 }
